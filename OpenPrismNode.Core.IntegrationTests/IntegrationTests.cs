860f0db06324f8404ce0400546f3b6d34b403b86
Björn Sandmann <sandmann@codedata.solutions>
2024-09-03 17:21:44 +0200

    Added additional test for rollback

--- OpenPrismNode.Core.IntegrationTests/IntegrationTests.cs
+++ OpenPrismNode.Core.IntegrationTests/IntegrationTests.cs
@@ -4,10 +4,12 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using OpenPrismNode.Core;
+using OpenPrismNode.Core.Commands.CreateAddresses;
 using OpenPrismNode.Core.Commands.CreateBlock;
 using OpenPrismNode.Core.Commands.CreateEpoch;
 using OpenPrismNode.Core.Commands.CreateLedger;
 using OpenPrismNode.Core.Commands.CreateStakeAddress;
+using OpenPrismNode.Core.Commands.CreateTransaction;
 using OpenPrismNode.Core.Commands.CreateTransactionCreateDid;
 using OpenPrismNode.Core.Commands.CreateTransactionDeactivateDid;
 using OpenPrismNode.Core.Commands.CreateTransactionUpdateDid;
@@ -20,9 +22,13 @@ using OpenPrismNode.Core.Commands.GetEpoch;
 using OpenPrismNode.Core.Commands.GetMostRecentBlock;
 using OpenPrismNode.Core.Commands.ResolveDid;
 using OpenPrismNode.Core.Common;
+using OpenPrismNode.Core.Crypto;
 using OpenPrismNode.Core.IntegrationTests;
 using OpenPrismNode.Core.Services;
+using OpenPrismNode.Sync.Commands.DecodeTransaction;
+using OpenPrismNode.Sync.Commands.ParseTransaction;
 using OpenPrismNode.Sync.Commands.ProcessBlock;
+using OpenPrismNode.Sync.Commands.ProcessTransaction;
 
 [Collection("TransactionalTests")]
 public partial class IntegrationTests : IDisposable
@@ -50,7 +56,12 @@ public partial class IntegrationTests : IDisposable
     private readonly IStakeAddressCache _stakeAddressCache;
     private readonly ResolveDidHandler _resolveDidHandler;
     private readonly ProcessBlockHandler _processBlockHandler;
+    private readonly ProcessTransactionHandler _processTransactionHandler;
+    private readonly DecodeTransactionHandler _decodeTransactionHandler;
     private readonly SwitchBranchHandler _switchBranchHandler;
+    private readonly CreateAddressesHandler _createAddressesHandler;
+    private readonly ParseTransactionHandler _parseTransactionHandler;
+    private readonly CreateTransactionHandler _createTransactionHandler;
 
     public IntegrationTests(TransactionalTestDatabaseFixture fixture)
     {
@@ -85,7 +96,13 @@ public partial class IntegrationTests : IDisposable
         this._createStakeAddressHandler = new CreateStakeAddressHandler(_context, _stakeAddressCache, Mock.Of<ILogger<CreateStakeAddressHandler>>());
         this._resolveDidHandler = new ResolveDidHandler(_context);
         this._processBlockHandler = new ProcessBlockHandler(_mediatorMock.Object, _appSettingsOptions, Mock.Of<ILogger<ProcessBlockHandler>>());
+        this._processTransactionHandler = new ProcessTransactionHandler(_mediatorMock.Object,Mock.Of<ILogger<ProcessTransactionHandler>>());
+        this._decodeTransactionHandler = new DecodeTransactionHandler();
         this._switchBranchHandler = new SwitchBranchHandler(_context, _mediatorMock.Object);
+        this._createAddressesHandler = new CreateAddressesHandler(_mediatorMock.Object);
+        this._parseTransactionHandler = new ParseTransactionHandler(_mediatorMock.Object, new Sha256ServiceBouncyCastle(), new EcServiceBouncyCastle(), Mock.Of<ILogger<ParseTransactionHandler>>());
+        this._createTransactionHandler = new CreateTransactionHandler(_mediatorMock.Object, new Sha256ServiceBouncyCastle());
+
     }
 
     public void Dispose()
--- OpenPrismNode.Core.IntegrationTests/Rollback/RollbackTests.cs
+++ OpenPrismNode.Core.IntegrationTests/Rollback/RollbackTests.cs
@@ -3,7 +3,12 @@ using FluentResults;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
+using OpenPrismNode.Core.Commands.CreateAddresses;
 using OpenPrismNode.Core.Commands.CreateBlock;
+using OpenPrismNode.Core.Commands.CreateStakeAddress;
+using OpenPrismNode.Core.Commands.CreateTransaction;
+using OpenPrismNode.Core.Commands.CreateTransactionCreateDid;
+using OpenPrismNode.Core.Commands.CreateWalletAddress;
 using OpenPrismNode.Core.Commands.GetBlockByBlockHash;
 using OpenPrismNode.Core.Commands.GetEpoch;
 using OpenPrismNode.Core.Commands.GetMostRecentBlock;
@@ -11,11 +16,16 @@ using OpenPrismNode.Core.Commands.SwitchBranch;
 using OpenPrismNode.Core.DbSyncModels;
 using OpenPrismNode.Core.Entities;
 using OpenPrismNode.Core.Models;
+using OpenPrismNode.Sync.Commands.DecodeTransaction;
+using OpenPrismNode.Sync.Commands.GetMetadataFromTransaction;
+using OpenPrismNode.Sync.Commands.GetPaymentDataFromTransaction;
 using OpenPrismNode.Sync.Commands.GetPostgresBlockByBlockId;
 using OpenPrismNode.Sync.Commands.GetPostgresBlockByBlockNo;
 using OpenPrismNode.Sync.Commands.GetPostgresBlockTip;
 using OpenPrismNode.Sync.Commands.GetTransactionsWithPrismMetadataForBlockId;
+using OpenPrismNode.Sync.Commands.ParseTransaction;
 using OpenPrismNode.Sync.Commands.ProcessBlock;
+using OpenPrismNode.Sync.Commands.ProcessTransaction;
 using OpenPrismNode.Sync.Services;
 
 public partial class IntegrationTests
@@ -490,7 +500,7 @@ public partial class IntegrationTests
         var baseBlockHash = new byte[] { 10, 1, 1, 1 };
         await SetupLedgerEpochAndBlock(ledgerType, epochNumber, blockHeight, baseBlockHash, null, 9);
 
-        // Setup the dbsync Block to be at block 12 to start syncing
+        // Setup the dbsync Block to be at block 11 to start syncing
         _mediatorMock.Setup(p => p.Send(It.IsAny<GetPostgresBlockTipRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(Result.Ok(new Block()
         {
             block_no = 11,
@@ -518,6 +528,203 @@ public partial class IntegrationTests
             .Returns(async (CreateBlockRequest request, CancellationToken token) => await this._createBlockHandler.Handle(request, token));
         _mediatorMock.Setup(p => p.Send(It.IsAny<SwitchBranchRequest>(), It.IsAny<CancellationToken>()))
             .Returns(async (SwitchBranchRequest request, CancellationToken token) => await this._switchBranchHandler.Handle(request, token));
+
+        // Return block 11 from dbsync 
+        _mediatorMock.SetupSequence(p => p.Send(It.IsAny<GetPostgresBlockByBlockNoRequest>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result.Ok(new Block()
+            {
+                block_no = 11,
+                epoch_no = 1,
+                hash = new byte[] { 11, 1, 1, 1 },
+                time = DateTime.UtcNow,
+                tx_count = 0,
+                id = 110,
+                previous_id = 100
+            }));
+
+        // Act 1
+        var syncResult = await SyncService.RunSync(_mediatorMock.Object, logger.Object, "preprod", new CancellationToken(), 10, false);
+
+        // Assert
+        syncResult.IsSuccess.Should().BeTrue();
+
+        // Blocks 10, 11 should be in the database
+        var blocks = await _context.BlockEntities.ToListAsync();
+        blocks.Should().Contain(p => p.BlockHeight == 10);
+        blocks.Should().Contain(p => p.BlockHeight == 11);
+
+
+        // Now the fork comes into play. When rerunning the sync-process, the tip is at block 11, but with different hash 
+        // It is referencing the same previousId (100) as the existing block 11 in the database
+        // Return block 11 from dbsync as the tip
+        _mediatorMock.Setup(p => p.Send(It.IsAny<GetPostgresBlockTipRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(Result.Ok(new Block()
+        {
+            block_no = 11,
+            epoch_no = 1,
+            hash = new byte[] { 11, 11, 11, 11 },
+            time = DateTime.UtcNow,
+            tx_count = 0,
+            id = 111,
+            previous_id = 100
+        }));
+
+        // Get data for block 11
+        _mediatorMock.SetupSequence(p => p.Send(It.IsAny<GetPostgresBlockByBlockNoRequest>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result.Ok(new Block()
+            {
+                block_no = 11,
+                epoch_no = 1,
+                hash = new byte[] { 11, 11, 11, 11 },
+                time = DateTime.UtcNow,
+                tx_count = 0,
+                id = 111,
+                previous_id = 100
+            }));
+
+        // Get block 10 from dbsync by its id
+        _mediatorMock.SetupSequence(p => p.Send(It.IsAny<GetPostgresBlockByBlockIdRequest>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result.Ok(new Block()
+            {
+                block_no = 10,
+                epoch_no = 1,
+                hash = new byte[] { 10, 1, 1, 1 },
+                time = DateTime.UtcNow,
+                tx_count = 0,
+                id = 100,
+                previous_id = 90
+            }));
+
+        var syncResult2 = await SyncService.RunSync(_mediatorMock.Object, logger.Object, "preprod", new CancellationToken(), 10, false);
+
+        // Assert
+        syncResult2.IsSuccess.Should().BeTrue();
+
+        // We should also now have a forked block 11 in the database
+        // But the block with the fork-flag is now the inital block 11 
+        // The branch as basically switched
+        var blocksforked = await _context.BlockEntities.Where(p => p.IsFork).ToListAsync();
+        blocksforked.Count.Should().Be(1);
+        blocksforked.Single().BlockHeight.Should().Be(11);
+        blocksforked.Single().BlockHashPrefix.Should().Be(BlockEntity.CalculateBlockHashPrefix(new byte[] { 11, 1, 1, 1 }));
+    }
+
+    [Fact]
+    public async Task Unknown_tip_with_identical_blockHeight_switches_fork_with_PRISM_transactions()
+    {
+        // Arrange block 10 as base
+        var logger = new Mock<ILogger>();
+        var ledgerType = LedgerType.CardanoPreprod;
+        var epochNumber = 1;
+        var blockHeight = 9;
+        blockHeight++;
+        var baseBlockHash = new byte[] { 10, 1, 1, 1 };
+        await SetupLedgerEpochAndBlock(ledgerType, epochNumber, blockHeight, baseBlockHash, null, 9);
+
+        // Setup the dbsync Block to be at block 11 to start syncing
+        _mediatorMock.Setup(p => p.Send(It.IsAny<GetPostgresBlockTipRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(Result.Ok(new Block()
+        {
+            block_no = 11,
+            epoch_no = 1,
+            hash = new byte[] { 11, 1, 1, 1 },
+            time = DateTime.UtcNow,
+            tx_count = 0,
+            previous_id = 100,
+            id = 110
+        }));
+
+        // Single PRISM transaction
+        _mediatorMock.Setup(p => p.Send(It.IsAny<GetTransactionsWithPrismMetadataForBlockIdRequest>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result.Ok(new List<Transaction>()
+            {
+                new Transaction()
+                {
+                    block_index = 7,
+                    fee = 34.34m,
+                    hash = new byte[] { 1, 1, 2, 2 },
+                    id = 8,
+                    size = 123
+                }
+            }));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<GetMetadataFromTransactionRequest>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result.Ok(new Metadata()
+            {
+                id = 9,
+                bytes = new byte[] { 2, 2, 3, 3 },
+                json = """
+                       {
+                       "c":[
+                       "0x22c60212c3020a076d61737465723012463044022007a66810de54b8362297afeffd6274f13576dba2385ac912dbda53bab13bb98102201179b6682c10d0e5e7",
+                       "0xe1c752e321b5d401b0cd23c68240128be7f2faed7228491aef010aec010ae90112390a056b65792d3110044a2e0a09736563703235366b31122102447a1746cc",
+                       "0xc1032b8471f3906b9b1ab507aa6ec6d2f25903a82ccce3d98a275112390a056b65792d3210024a2e0a09736563703235366b31122103b07d9cade268680a502e",
+                       "0x3d20aacb79bd283ab2cf8b5703d3cc5e7be9626bcb7b123b0a076d61737465723010014a2e0a09736563703235366b31122102888a4ccec8d6309d3c2e4adcb0",
+                       "0x46b7808608660c69340968a927013aa7b2002c1a340a09736572766963652d31120d4c696e6b6564446f6d61696e731a185b2268747470733a2f2f6d342e6373",
+                       "0x69676e2e696f2f225d"
+                       ],
+                       "v":1
+                       }
+                       """,
+                key = 21235,
+                tx_id = 8
+            }));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<GetPaymentDataFromTransactionRequest>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result.Ok(new Payment()
+            {
+                Incoming = new List<Utxo>()
+                {
+                    new Utxo()
+                    {
+                        Index = 0,
+                        WalletAddress = new WalletAddress()
+                        {
+                            StakeAddressString = null,
+                            WalletAddressString = "someWalletAddress1"
+                        },
+                        Value = 123
+                    }
+                },
+                Outgoing = new List<Utxo>()
+                {
+                    new Utxo()
+                    {
+                        Index = 1,
+                        WalletAddress = new WalletAddress()
+                        {
+                            StakeAddressString = "someStakeAddress",
+                            WalletAddressString = "someWalletAddress2"
+                        },
+                        Value = 456
+                    }
+                }
+            }));
+
+        _mediatorMock.Setup(p => p.Send(It.IsAny<DecodeTransactionRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (DecodeTransactionRequest request, CancellationToken token) => await this._decodeTransactionHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<ProcessTransactionRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (ProcessTransactionRequest request, CancellationToken token) => await this._processTransactionHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<GetMostRecentBlockRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (GetMostRecentBlockRequest request, CancellationToken token) => await this._getMostRecentBlockHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<GetBlockByBlockHashRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (GetBlockByBlockHashRequest request, CancellationToken token) => await this._getBlockByBlockHashHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<GetEpochRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (GetEpochRequest request, CancellationToken token) => await this._getEpochHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<ProcessBlockRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (ProcessBlockRequest request, CancellationToken token) => await this._processBlockHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<CreateBlockRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (CreateBlockRequest request, CancellationToken token) => await this._createBlockHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<SwitchBranchRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (SwitchBranchRequest request, CancellationToken token) => await this._switchBranchHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<CreateAddressesRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (CreateAddressesRequest request, CancellationToken token) => await this._createAddressesHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<CreateWalletAddressRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (CreateWalletAddressRequest request, CancellationToken token) => await this._createWalletAddressHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<CreateStakeAddressRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (CreateStakeAddressRequest request, CancellationToken token) => await this._createStakeAddressHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<ParseTransactionRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (ParseTransactionRequest request, CancellationToken token) => await this._parseTransactionHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<CreateTransactionRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (CreateTransactionRequest request, CancellationToken token) => await this._createTransactionHandler.Handle(request, token));
+        _mediatorMock.Setup(p => p.Send(It.IsAny<CreateTransactionCreateDidRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(async (CreateTransactionCreateDidRequest request, CancellationToken token) => await this._createTransactionCreateDidHandler.Handle(request, token));
 
         // Return block 11 from dbsync 
         _mediatorMock.SetupSequence(p => p.Send(It.IsAny<GetPostgresBlockByBlockNoRequest>(), It.IsAny<CancellationToken>()))
